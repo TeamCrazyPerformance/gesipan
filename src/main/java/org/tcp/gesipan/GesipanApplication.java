76f34a9b3efe24b43565a4846789df5abe4b086c
geine <bonos2@wisdo.me>
2018-09-14 00:39:59 +0900

    initial commit

---
+++ src/main/java/org/tcp/gesipan/GesipanApplication.java
@@ -0,0 +1,12 @@
+package org.tcp.gesipan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesipanApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GesipanApplication.class, args);
+  }
+}
---
+++ src/test/java/org/tcp/gesipan/GesipanApplicationTests.java
@@ -0,0 +1,16 @@
+package org.tcp.gesipan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GesipanApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
