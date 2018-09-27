c15faab2538f8677524b0cb37c2783220cfcb048
sklim0932 <sklim0932@gmail.com>
2018-09-27 19:28:25 +0900

    Add View files

--- src/main/java/org/tcp/gesipan/GesipanApplication.java
+++ src/main/java/org/tcp/gesipan/GesipanApplication.java
@@ -2,10 +2,12 @@ package org.tcp.gesipan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GesipanApplication {
   public static void main(String[] args) {
     SpringApplication.run(GesipanApplication.class, args);
   }
+
 }
